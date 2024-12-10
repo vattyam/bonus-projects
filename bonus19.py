419543389155a36f87ab35ccaa1ff587efad637b
sandeep <vattyam.sandeep@gmail.com>
2024-12-10 10:51:19 +0530

    Initial commit

---
+++ bonus19.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from PIL import Image
+
+with st.expander("Start Camera"):
+    #start camera
+    camera_image = st.camera_input("Camera")
+
+#print(camera_image)
+
+if camera_image:
+    # Create a pillow image instance
+    img = Image.open(camera_image)
+
+    # Convert the pillow image to grayscale
+    gray_img = img.convert("L")
+
+    # Render the grayscale image on the webpage
+    st.image(gray_img)
+
